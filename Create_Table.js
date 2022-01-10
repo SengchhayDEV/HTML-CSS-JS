8b23d2538a331a8c3a3113288969d9befc1ddee8
Sengchhay Ly <96579097+SengchhayDEV@users.noreply.github.com>
2022-01-10 19:32:46 +0700

    Create Create_Table.js

---
+++ Create_Table.js
@@ -0,0 +1,33 @@
+const table=[
+    { 
+        Serie:"Iphone5",
+        instock: 999,
+        price:"free",
+        Accessories:"none"
+    },
+    {
+        Serie:"Iphone5s",
+        instock: 999,
+        price:"25$",
+        Accessories:"Headphone & Charger"
+    },
+    {
+        Serie:"Iphone6",
+        instock: 200,
+        price:"50$",
+        Accessories:"Headphone & Charger"
+    },
+    {
+        Serie:"Iphone7",
+        instock: 100,
+        price:"180$",
+        Accessories:"Headphone & Charger"
+    },
+    {
+        Serie:"Iphone8",
+        instock: 50,
+        price:"250$",
+        Accessories:"Headphone & Charger"
+    },
+]
+console.table(table);
